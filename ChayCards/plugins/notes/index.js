49c4f9d9d07bc77288aed54b2e4e588392211647
Inkmortal <studio@seadragoninkmortal.com>
2024-10-25 11:12:59 -0700

    Nuke

--- ChayCards/plugins/notes/index.js
+++
@@ -1,2 +0,0 @@
-export { default as NotesPlugin } from './plugin';
-export type { NoteDocument } from './types';
--- ChayCards/plugins/notes/schema.js
+++
@@ -1,7 +0,0 @@
-export const notesSchema = `
-  CREATE TABLE IF NOT EXISTS notes (
-    id TEXT PRIMARY KEY REFERENCES documents(id),
-    content TEXT NOT NULL,
-    word_count INTEGER NOT NULL DEFAULT 0
-  );
-`;
